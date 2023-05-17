f07952a66183b0c53f0e8038e4a2c85289f04bf1
Imane Allay <iallay@xpanxion.com>
2023-05-17 14:07:24 -0400

    Add ex2

--- src/Student6/Ex2/Main.js
+++ src/Student6/Ex2/Main.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+
+import "./Main.css";
+
+import { Header } from "./Header";
+
+import { Menu } from "./Menu";
+
+import { Footer } from "./Footer";
 
 export const Main = () => {
   return (
-    <div>
-        Hello I'm student id: 1.
+    <div className="Main">
+      <Header />
+      <Menu />
+      <Footer />  
     </div>
-  )
-}
+  );
+};
--- src/Student6/Ex3/Main.js
+++ src/Student6/Ex3/Main.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Main.css'
+import { Tweet } from './Tweet'
 
 export const Main = () => {
   return (
     <div>
-        Hello I'm student id: 1.
+        <div className="Main">
+        <Tweet message="It's payday!!!" color="green"/>
+    <Tweet message="I just bought a new car!" color="yellow" />
+    <Tweet message="I just smashed my new car." color="red" />
+</div>
     </div>
   )
 }
+
+export default Main;
