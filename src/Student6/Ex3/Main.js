e86adffa9db35fabcd7bac184bda18ceb033b4c5
Imane Allay <iallay@xpanxion.com>
2023-05-17 17:25:10 -0400

    Add Ex3

--- src/Student6/Ex3/Main.js
+++ src/Student6/Ex3/Main.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import './Main.css'
-import { Tweet } from './Tweet'
+import { Tweets } from './Tweets'
 
 export const Main = () => {
   return (
-    <div>
-        <div className="Main">
-        <Tweet message="It's payday!!!" color="green"/>
-    <Tweet message="I just bought a new car!" color="yellow" />
-    <Tweet message="I just smashed my new car." color="red" />
-</div>
-    </div>
+   
+       <Tweets/>
+
+  
   )
 }
 
-export default Main;
